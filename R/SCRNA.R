2b489ee1bf4cb83be1cf0040d326c7ac6ff90082
Matt Heffernan <mheffe3@uic.edu>
2021-08-20 12:59:57 -0500

    Restart

--- R/SCRNA.R
+++ R/SCRNA.R
@@ -1,15 +1,22 @@
 
 # library(Seurat)
 # library(scPPIN)
-# library(data.table)
 # library(kutils)
 # library(RCy3)
+
+
+
+
+
+
 
 #' @import Seurat
 #' @import scPPIN
 #' @import RCy3
 #' @importFrom kutils writeCSV
-#' 
+
+
+
 #' @export
 getFile <- function(filepath)
 {
@@ -18,6 +25,7 @@ getFile <- function(filepath)
     if(grepl(".csv", filepath, fixed = T))
     {
       Data <- read.csv(filepath, header = T, row.names = 1)
+      
     }
     else if(grepl(".txt", filepath, fixed = T))
     {
@@ -38,6 +46,10 @@ getFile <- function(filepath)
   return(Data)
   
 }
+
+
+
+
 
 #' @export
 Preprocess <- function(Data)
@@ -66,6 +78,8 @@ Preprocess <- function(Data)
 }
 
 
+
+
 #' @export
 getPValues <- function(SData)
 {
@@ -79,23 +93,42 @@ getPValues <- function(SData)
 
 
 #' @export
-getClusterList <- function(Data, PValues)
+getClusterList <- function(SData, PValues, names = NULL)
 {
-  nClusters = nlevels(seqwell@meta.data$seurat_clusters)
+  nClusters = nlevels(SData@meta.data$seurat_clusters)
   ClusterList <- list()
   i = 0
-  while(i < nClusters)
+  if(!is.null(names) && length(names) == nClusters)
   {
-    ClusterList[[paste("C",as.character(i),sep="")]] = PValues[PValues$cluster==i,]
-    i = i+1
+    while(i < nClusters)
+    {
+      ClusterList[[names[i]]] = PValues[PValues$cluster==i]
+      i = i+1
+      
+    }
+   
   }
+  
+  else
+  {
+    warning("EITHER NO NAMES WERE GIVEN OR CHARCTER VECTOR PROVIDED IS NOT THE SAME LENGTH AS THE NUMBER OF CLUSTERS. USING C-ith FOR NAMES")
+    while(i < nClusters)
+    {
+      ClusterList[[paste("C",as.character(i),sep="")]] = PValues[PValues$cluster==i,]
+      i = i+1
+    }
+  }
+  
   return(ClusterList)
   
 }
 
 
+
+
+
 #' @export
-FM <- function(pvalues)
+getFM <- function(pvalues)
 {
   ppin <- loadPPIN()
   checksum = sum(pvalues<0) + sum(pvalues>1)
@@ -111,9 +144,9 @@ FM <- function(pvalues)
 
 
 #' @export
-getEdgeWeights <- function(functionalModule, countTable)
+getEdgeWeightsF <- function(FM, countTable)
 {
-  network <- as.data.frame(get.edgelist(functionalModule))
+  network <- as.data.frame(get.edgelist(FM))
   colnames(network) <- c('TF','GENE')
   Rho <- vector()
   
@@ -125,8 +158,7 @@ getEdgeWeights <- function(functionalModule, countTable)
     G2 = network[i,2]
     G1C <- countTable[G1,]
     G2C <- countTable[G2,]
-    corrData <- cor.test(G1C,G2C,method="spearman", exact=FALSE)
-    corr <- unname(corrData$estimate)
+    corr <- unname(cor.test(G1C,G2C,method="spearman", exact=FALSE)$estimate)
     Rho[i] <- corr
     i = i+1
   }
@@ -135,31 +167,92 @@ getEdgeWeights <- function(functionalModule, countTable)
 }
 
 
+
+
 #' @export
-SendToCSV <- function(functionalModule, network, Ps, name)
+getEdgeWeightsG <- function(FM, countTable)
 {
-  filename = paste(name, "N.csv", sep="")
-  writeCSV(network, filename, row.names = FALSE)
+  network <- as.data.frame(get.edgelist(FM))
   
-  filename = paste(name, "E.csv", sep="")
-  table <- as_data_frame(functionalModule,what=c("vertices"))
-  writeCSV(table2, filename, row.names = FALSE)
+  nEdges <- nrow(network)
+  i=1
+  while(i <= nEdges)
+  {
+    G1 = network[i,1]
+    G2 = network[i,2]
+    G1C <- countTable[G1,]
+    G2C <- countTable[G2,]
+    corr <- unname(cor.test(G1C,G2C,method="spearman", exact=FALSE)$estimate)
+    FM <- set_edge_attr(FM, "weight", index = E(FM, P = c(G1,G2),directed = FALSE), value = corr)
+    i = i+1
+  }
+  return(FM)
+}
+
+
+
+#' @export
+SendToCSV <- function(functionalModule, network, Ps, name = C)
+{
+    filename = paste(name, "N.csv", sep="")
+    writeCSV(network, filename, row.names = FALSE)
+    
+    filename = paste(name, "E.csv", sep="")
+    table <- as_data_frame(functionalModule,what=c("vertices"))
+    writeCSV(table2, filename, row.names = FALSE)
   
 }
 
 
+
 #' @export
-getDataFiles <- function(SData, Ps)
+GraphInCytoscape <- function(FM, name)
 {
+  createNetworkFromIgraph(FM, title= name)
+  
+  setNodeColorMapping('nodeScore', c(min(V(FM)$nodeScore), mean(V(FM)$nodeScore), max(V(FM)$nodeScore)), c('#F5EDDD', '#F59777', '#F55333'))
+  TFs <- readLines("TFs.txt")
+  table <- as_data_frame(FM,what=c("vertices"))
+  filtered <- table[table$name %in% TFs,]
+  TF <- filtered[,'name']
+  setNodeShapeBypass(node.names=TF, new.shapes="ELLIPSE")
+  
+  
+  setEdgeLineWidthMapping('weight',c(min(E(FM)$weight), mean(E(FM)$weight), max(E(FM)$weight)), widths = c(1,10,20))
+  setEdgeColorMapping('weight',c(min(E(FM)$weight), mean(E(FM)$weight), max(E(FM)$weight)), colors= c("#00008B", "#FFFFFF", "#EE4B2B"))
+  
+  
+}
+
+
+
+
+#' @export
+CyFilter <- function(FM, name, NSLB = 100, EWLB = 0.05)
+{
+  createColumnFilter(filter.name="Node Score Filter", column = "nodeScore", c(NSLB, max(V(FM0)$nodeScore)), predicate = "BETWEEN")
+  createColumnFilter(filter.name = "Edge Weight Filter", column = "weight", c(-EWLB, EWLB), predicate = "IS_NOT_BETWEEN", type = "edges")
+  createCompositeFilter(filter.name = "Edge Weight & Node Score Filter", c("Node Score Filter", "Edge Weight Filter"), "ANY")
+  createSubnetwork(subnetwork.name = paste(name, "Filtered Network", sep=""))
+  
+}
+
+
+
+
+#' @export
+getDataFiles <- function(SData, Ps, cnames = NULL)
+{
+  
   countTable <- SData[["RNA"]]@data
-  CList <- getClusterList(SData, Ps)
+  CList <- getClusterList(SData, Ps, cnames)
   FMList <- list()
   i = 0
   for(c in CList)
   {
     pValues <- c$p_val
     names(pValues) <- c$gene
-    FMList[[paste("FM", as.character(i),sep="")]] <- FM(pValues)
+    FMList[[paste("FM", as.character(i),sep="")]] <- getFM(pValues)
     i = i+1
     
   }
@@ -167,26 +260,105 @@ getDataFiles <- function(SData, Ps)
   i = 0
   for(f in FMList)
   {
-    networkList[[paste("N", as.character(i),sep="")]] <- getEdgeWeights(f,countTable)
+    networkList[[paste("N", as.character(i),sep="")]] <- getEdgeWeightsF(f,countTable)
     i = i+1
     
   }
   
   i = 1
   nClusters <- length(CList)
+  hasnames = TRUE
+  if(is.null(cnames) || length(cnames) != nClusters)
+  {
+    hasnames = FALSE
+  }
   while(i <= nClusters)
   {
-    SendToCSV(FMList[[i]],networkList[[i]],CList[[i]],(i-1))
+    if(hasnames)
+    {
+      SendToCSV(FMList[[i]],networkList[[i]],CList[[i]], name= cnames[i])
+      
+    }
+    else
+    {
+      SendToCSV(FMList[[i]],networkList[[i]],CList[[i]], name= paste("C", as.character(i-1), sep=""))
+    }
+    
     i = i+1
   }
   
 }
 
 
+
+
+
+
+
+#' @export
+getCyGraphs <- function(SData, Ps,NSLB = 100, EWLB = 0.05, cnames = NULL)
+{
+  countTable <- SData[["RNA"]]@data
+  CList <- getClusterList(SData, Ps, cnames)
+  FMList <- list()
+  i = 0
+  for(c in CList)
+  {
+    pValues <- c$p_val
+    names(pValues) <- c$gene
+    FMList[[paste("FM", as.character(i),sep="")]] <- getFM(pValues)
+    i = i+1
+    
+  }
+  networkList <- list()
+  i = 0
+  for(f in FMList)
+  {
+    networkList[[paste("N", as.character(i),sep="")]] <- getEdgeWeightsG(f,countTable)
+    i = i+1
+    
+  }
+  
+  i = 1
+  nClusters <- length(CList)
+  hasnames = TRUE
+  if(is.null(cnames) || length(cnames) != nClusters)
+  {
+    hasnames = FALSE
+  }
+  while(i <= nClusters)
+  {
+    if(hasnames)
+    {
+      GraphInCytoscape(FMList[[i]], name=cnames[i])
+      CyFilter(FMList[i], name=cnames[i], NSLB = NSLB, EWLB = EWLB)
+      
+    }
+    else
+    {
+      GraphInCytoscape(FMList[[i]], name=paste("C", as.character(i-1), sep=""))
+      CyFilter(FM, name=paste("C", as.character(i-1), sep=""), NSLB = NSLB, EWLB = EWLB)
+    }
+    
+    i = i+1
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
 #Function Calls
 
 
-# uterusData <- getFile("GSM4008666_Adult-Uterus1_dge.txt")
-# seqwell <- Preprocess(uterusData)
-# PValues <- getPValues(seqwell)
-# getDataFiles(SData = seqwell, Ps = PValues)
+# Data <- getFile("GSM4008666_Adult-Uterus1_dge.txt")
+# SData <- Preprocess(Data)
+# PValues <- getPValues(SData)
+# getDataFiles(SData, PValues)
+# getCyGraphs(SData, PValues, 100, 0.05)
--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Single Cell Network Visualization in Cytoscape
 
 # Dependencies
 Seurat  
-scPPIN  
+scPPIN  devtools::install_github("flocklimm/scPPIN")  
 kutils  
 RCy3  
+
+# Installation
+devtools::install_github("mheffe3/SCNVC")  
